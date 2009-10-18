ae5acee3187a1096594630dfc8beec31bbc07f57
Ricardo Signes <rjbs@cpan.org>
2009-10-18 10:59:47 -0400

    test nested element

---
+++ lib/Pod/Elemental/Element/Nested.pm
@@ -0,0 +1,20 @@
+package Pod::Elemental::Element::Nested;
+use Moose;
+extends 'Pod::Elemental::Element::Generic::Command';
+with 'Pod::Elemental::Node';
+
+use Moose::Autobox 0.10;
+
+override as_pod_string => sub {
+  my ($self) = @_;
+
+  my $string = super;
+
+  join q{},
+    "$string\n",
+    $self->children->map(sub { $_->as_pod_string })->flatten;
+};
+
+use namespace::autoclean;
+
+1;
