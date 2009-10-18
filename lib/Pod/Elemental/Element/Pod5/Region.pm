e33a438120ea3e54572cad9a4121e07defb4fb05
Ricardo Signes <rjbs@cpan.org>
2009-10-18 08:34:09 -0400

    move FormatRegion guts to Region

--- lib/Pod/Elemental/Element/Pod5/Region.pm
+++ lib/Pod/Elemental/Element/Pod5/Region.pm
@@ -4,11 +4,40 @@ use Moose;
 with qw(
   Pod::Elemental::Paragraph
   Pod::Elemental::Node
-  Pod::Elemental::FormatRegion
   Pod::Elemental::Command
 );
 
 use Moose::Autobox;
+
+use Pod::Elemental::Types qw(FormatName);
+use MooseX::Types::Moose qw(Bool);
+
+=attr format_name
+
+This is the format to which the document was targeted.  By default, this is
+undefined and the document is vanilla pod.  If this is set, the document may or
+may not be pod, and is intended for some other form of processor.  (See
+L</is_pod>.)
+
+=cut
+
+has format_name => (is => 'ro', isa => FormatName, required => 1);
+
+=attr is_pod
+
+If true, this document contains pod paragraphs, as opposed to data paragraphs.
+This will generally result from the document originating in a C<=begin> block
+with a colon-prefixed target identifier:
+
+  =begin :html
+
+    This is still a verbatim paragraph.
+
+  =end :html
+
+=cut
+
+has is_pod => (is => 'ro', isa => Bool, required => 1, default => 1);
 
 sub command         { 'begin' }
 sub closing_command { 'end' }
