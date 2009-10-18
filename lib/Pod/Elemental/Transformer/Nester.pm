fb8d25ce121742575a06886a48a9dc89f14ce16f
Ricardo Signes <rjbs@cpan.org>
2009-10-18 10:01:19 -0400

    tweak how things are defined

--- lib/Pod/Elemental/Transformer/Nester.pm
+++ lib/Pod/Elemental/Transformer/Nester.pm
@@ -6,12 +6,14 @@ with 'Pod::Elemental::Transformer';
 use namespace::autoclean;
 
 # so here you'll specify something like:
-#   head1  => [ things it can contain ]
-#   method => [ things it can contain ]
+#   head1  => [ things it can contain / things it can't contain ]
+#   method => [ things it can contain / things it can't contain ]
 # and it will go through the list of paragraphs, and nest all the "things a
 # head1 can contain" under the head1, if they are subsequent to it
 #
 # we'll enforce the rule that nothing that can be contained can also be a
 # container, if possible; probably not possible :-/
+
+# DEFAULT: head1 => [ allow: =head2.., =over, =item, =back, *text ]
 
 1;
