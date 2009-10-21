e2118a40b2e9640e5c75b9e429c4096fe1700611
Ricardo Signes <rjbs@cpan.org>
2009-10-21 00:07:09 -0400

    more docs

--- lib/Pod/Elemental/Transformer/Gatherer.pm
+++ lib/Pod/Elemental/Transformer/Gatherer.pm
@@ -9,15 +9,78 @@ use Moose::Autobox 0.10;
 use MooseX::Types::Moose qw(CodeRef);
 use Pod::Elemental::Node;
 
-# so here you'll specify something like:
-#   find all =method paragraphs in the list and put them under a new node (like
-#   =head1 METHODS) and put that in place of the first =method paragraph
+=head1 OVERVIEW
+
+Like the Nester transformer, this Gatherer produces structure and containment
+in a Pod document.  Unlike that Nester, it does not find top-level elements,
+but instead produces them.
+
+It looks for all elements matching the C<gather_selector>.  They are removed
+from the node.  In the place of the first found element, the C<container> node
+is placed into the transformed node, and all the gathered elements are made
+children of the container.
+
+So, given this document:
+
+  Document
+    =head1 Foo
+    =over 4
+    =item * xyzzy
+    =item * abcdef
+    =back
+    =head1 Bar
+    =over 4
+    =item * 1234
+    =item * 8765
+    =back
+
+...and this nester...
+
+  my $nester = Pod::Elemental::Transformer::Gatherer->new({
+    gather_selector => s_command( [ qw(over item back) ] ),
+    container       => Pod::Elemental::Element::Pod5::Command->new({
+      command => 'head1',
+      content => "LISTS\n",
+    }),
+  });
+
+Then this:
+
+  $nester->transform_node($document);
+
+Will result in this document:
+
+  Document
+    =head1 Foo
+    =head1 LISTS
+      =over 4
+      =item * xyzzy
+      =item * abcdef
+      =back
+      =over 4
+      =item * 1234
+      =item * 8765
+      =back
+    =head1 Bar
+
+=attr gather_selector
+
+This is a coderef (a predicate) used to find the paragraphs to gather up.
+
+=cut
 
 has gather_selector => (
   is  => 'ro',
   isa => CodeRef,
   required => 1,
 );
+
+=attr container
+
+This is a Pod::Elemental::Node that will be inserted into the node, containing
+all gathered elements.
+
+=cut
 
 has container => (
   is   => 'ro',
--- lib/Pod/Elemental/Transformer/Nester.pm
+++ lib/Pod/Elemental/Transformer/Nester.pm
@@ -3,6 +3,53 @@ use Moose;
 with 'Pod::Elemental::Transformer';
 # ABSTRACT: group the document into sections
 
+=head1 OVERVIEW
+
+The Nester transformer is meant to find potential container elements and make
+them into actual containers.  It works by being told what elements may be made
+into containers and what subsequent elements they should allow to be stuffed
+into them.
+
+For example, given the following nester:
+
+  use Pod::Elemental::Selectors qw(s_command s_flat);
+
+  my $nester = Pod::Elemental::Transformer::Nester->new({
+    top_selector      => s_command('head1'),
+    content_selectors => [
+      s_command([ qw(head2 head3 head4) ]),
+      s_flat,
+    ],
+  });
+
+..then when we apply the transformation:
+
+  $nester->transform_node($document);
+
+...the nester will find all C<=head1> elements in the top-level of the
+document.  It will ensure that they are represented by objects that perform the
+Pod::Elemental::Node role, and then it will move all subsequent elements
+matching the C<content_selectors> into the container.
+
+So, if we start with this input:
+
+  =head1 Header
+  =head2 Subheader
+  Pod5::Ordinary <some content>
+  =head1 New Header
+
+The nester will convert its structure to look like this:
+
+  =head1 Header
+    =head2 Subheader
+    Pod5::Ordinary <some content>
+  =head1 New Header
+
+Once an element is reached that does not pass the content selectors, the
+nesting ceases until the next potential container.
+
+=cut
+
 use Moose::Autobox 0.10;
 use MooseX::Types::Moose qw(ArrayRef CodeRef);
 
@@ -11,22 +58,27 @@ use Pod::Elemental::Selectors -all;
 
 use namespace::autoclean;
 
-# so here you'll specify something like:
-#   head1  => [ things it can contain / things it can't contain ]
-#   method => [ things it can contain / things it can't contain ]
-# and it will go through the list of paragraphs, and nest all the "things a
-# head1 can contain" under the head1, if they are subsequent to it
-#
-# we'll enforce the rule that nothing that can be contained can also be a
-# container, if possible; probably not possible :-/
-#
-# DEFAULT: head1 => [ allow: =head2.., =over, =item, =back, *text ]
+=attr top_selector
+
+This attribute must be a coderef (presumably made from
+Pod::Elemental::Selectors) that will test elements in the transformed node and
+return true if the element is a potential new container.
+
+=cut
 
 has top_selector => (
   is  => 'ro',
   isa => CodeRef,
   required => 1,
 );
+
+=attr content_selectors
+
+This attribute must be an arrayref of coderefs (again presumably made from
+Pod::Elemental::Selectors) that will test whether paragraphs subsequent to the
+top-level container may be moved under the container.
+
+=cut
 
 has content_selectors => (
   is  => 'ro',
